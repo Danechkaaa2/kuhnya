0bc1af7eefbf88463c4f98170b1155ce5bd60ca8
Danechkaaa2 <dobrovolskiytube@gmail.com>
2025-05-02 03:11:12 +0500

    Add files via upload

---
+++ mebeldanya/kitchens/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('reviews/', views.reviews, name='reviews'),
+]
